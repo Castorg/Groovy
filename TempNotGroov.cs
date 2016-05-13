a0824572cfe72a21c32cc8f08546c9ae248dd37b
Pitkevich Maxim <rumata.it@gmail.com>
2016-05-13 09:27:48 +0300

    Update TempNotGroov.cs

--- TempNotGroov.cs
+++ TempNotGroov.cs
@@ -2,18 +2,27 @@
     {
         public string Country { get; set; }
         public string City { get; set; }
-        public string Adress { get; set; }
+        public string Address { get; set; }
         public int Stars { get; set; }
         public virtual List<RoomEntity> Rooms { get; set; }
         public virtual NotPrimaryServicesEntity NotPrimaryServices { get; set; }
 
     }
 
+    public class ParkingEntity
+    {
+        public int CountPlaces { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal BasicCostPerQuantum { get; set; }
+    }
+
     public class RoomEntity : BaseEntity
     {
         public int Floor { get; set; }
         public int RoomNumber { get; set; }
-        public decimal BasicCostPerDay { get; set; }
+        public decimal BasicCostPerQuantum { get; set; }
         public RoomState RoomState { get; set; }
         public int QuantityOfPlaces { get; set; }
         public string Notes { get; set; }
@@ -30,20 +39,34 @@
 
     public class CleaningServieEntity
     {
-        
+        public string CleaningType { get; set; }
+        public decimal BasicCostPerQuantum { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class InterestPlacesEntity
+    {
+        public string PlaceName { get; set; }
+        public string Site { get; set; }
+        public DayOfWeek WorkDay { get; set; }
+        public decimal BasicCostPerQuantum { get; set; }
+        public virtual ParkingEntity NearParking { get; set; }
     }
 
     public class ExcursionEntity : BaseEntity
     {
         public string ExcursionName { get; set; }
 
+        public decimal BasicCostPerQuantum { get; set; }
 
     }
 
     
     public class FoodServiceEntity
     {
-        public string ExcursionName { get; set; }
+        public string Name { get; set; }
+
+
     }
     public class TourEntity
     {
@@ -53,7 +76,7 @@
         public virtual string HotelEntity { get; set; }
     }
 
-    public class BookingEntity : BaseEntity
+    public class BookingEntity
     {
         public string BookingState { get; set; }
         public DateTime StartBookingTime { get; set; }
